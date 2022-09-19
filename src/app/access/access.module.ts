21ac8adb2f746abb03353f5245a50e121a446ad0
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-19 21:49:02 +0300

    updated register comp

--- src/app/access/access.module.ts
+++ src/app/access/access.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { AccessRoutingModule } from './access-routing.module';
 import { RegisterComponent } from './register/register.component';
+import { MaterialModule } from '../material.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { RegisterComponent } from './register/register.component';
   ],
   imports: [
     CommonModule,
-    AccessRoutingModule
+    AccessRoutingModule,
+    MaterialModule
   ]
 })
 export class AccessModule { }
--- src/app/access/register/register.component.ts
+++ src/app/access/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  redirectLogin() {
+    this.router.navigate(['login']);
+  }
+
+  reactiveForm = new FormGroup({
+    userid: new FormControl(''),
+    name: new FormControl(''),
+    password: new FormControl(''),
+    email: new FormControl(''),
+  })
 
 }
