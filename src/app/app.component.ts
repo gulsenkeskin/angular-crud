965d8c3274da9a9a815f3cba44c7a797c4e89ab2
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-19 21:16:04 +0300

    login ekranını açınca menüyü gizlemek

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements DoCheck {
+  constructor(private route: Router) { }
+
   title = 'angular-crud';
+  isMenuVisible: boolean = true;
+
+  ngDoCheck(): void {
+    const currentRoute = this.route.url;
+    if (currentRoute === '/login') {
+      this.isMenuVisible = false;
+    } else {
+      this.isMenuVisible = true;
+    }
+  }
 }
