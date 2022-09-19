fc6daa03717947c5786d722d30be7abd52aee2aa
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-19 11:44:10 +0300

    added status comp in app html

--- src/app/status/status.component.ts
+++ src/app/status/status.component.ts
@@ -3,11 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-status',
   template: `
-    <p>
-      status works!
-    </p>
+    <h4>
+      Angular
+    </h4>
   `,
   styles: [
+    "h4{color:green}"
   ]
 })
 export class StatusComponent implements OnInit {
