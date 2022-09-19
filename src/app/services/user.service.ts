2cc28fa2869a2ab0907b76dc7a7e3e0b4175f5ae
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-19 18:12:47 +0300

    upd

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  proceedLogin(inputData: any) {
+
+    return this.http.post(`${environment.baseUrl}/User/Authenticate`, inputData);
+  }
+
 }
