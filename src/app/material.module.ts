1f3d2708026f03c4457be0ad02faaa504f126ef6
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-19 16:15:42 +0300

    Update material.module.ts

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   exports: [
-    MatCardModule
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatTableModule
   ]
 })
 export class MaterialModule { }
