3017882b80dcada35cc17f23d9cdc7420cd39edd
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-19 11:41:24 +0300

    added status comp with ng g c status --inline-style --inline-template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { StatusComponent } from './status/status.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { ContactComponent } from './contact/contact.component';
     HomeComponent,
     AboutComponent,
     ContactComponent,
+    StatusComponent,
   ],
   imports: [
     BrowserModule,
